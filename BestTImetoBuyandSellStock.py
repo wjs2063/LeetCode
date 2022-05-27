7c5d5c57090f2b1f1d83985dde57e8478445d84f
jeonjaehyeon <wjs2063@naver.com>
2022-05-28 02:20:03 +0900

    DynamicProgramming

---
+++ BestTImetoBuyandSellStock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price=prices[0]
+        max_profit=0
+        for i in range(1,len(prices)):
+            min_price=min(min_price,prices[i])
+            max_profit=max(max_profit,prices[i]-min_price)
+        return max_profit
+
+# price-> po,p1,p2,p3,p4,p5 . . . 
