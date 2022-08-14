b7d1c9d3f1d4d4a4921f221985f3f15a4ebe178b
Jae hyeon <wjs2063@naver.com>
2022-08-14 20:29:34 +0900

    Array

--- ContainsDuplicate2.py
+++ ContainsDuplicate2.py
@@ -13,3 +13,17 @@ class Solution:
         return False
 
 # 각 value 에 해당하는 key 값들을 다모아본다 그리고 비교해봄
+
+from collections import defaultdict
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        cash_table = defaultdict( lambda :-10**5 +1)
+        for index,num in enumerate(nums) :
+            if index - cash_table[num] <= k :
+                return True
+            else:
+                cash_table[num] = index
+        return False
+        
+
+# 각 value 에 해당하는 key 값들을 다모아본다 그리고 비교해봄
