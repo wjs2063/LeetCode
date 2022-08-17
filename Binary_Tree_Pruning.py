ac3d7fbd023c6b9114dca22054d83c8388a3ee02
Jae hyeon <wjs2063@naver.com>
2022-08-17 11:57:20 +0900

    dfs

---
+++ Binary_Tree_Pruning.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def dfs(root):
+            
+            if root == None :
+                return None
+            # 왼쪽자식과 오른쪽자식에 dfs 똑같이 적용
+            # 해당함수는 먼저 쭉 ! 분기한후에 거꾸로 돌아오는방식으로 동작 될것
+            root.left = dfs(root.left)
+            root.right = dfs(root.right)
+            
+            # left,child 가 다 없고 root.val 이 0이면 None 으로 바꿔주자
+            if not root.left and not root.right and not root.val :
+                return None
+            return root
+        root = dfs(root)
+        return root
